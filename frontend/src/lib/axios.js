86ef51b5cd8ae950fac3387ee3d73aa5d8411886
MUHAMMED SIYAD T <146119724+Muhammedsiyadt@users.noreply.github.com>
2025-05-13 09:49:04 +0530

    changed axios url backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://13.238.120.32:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "/" : "http://13.238.120.32:5001/api",
   withCredentials: true,
 });
