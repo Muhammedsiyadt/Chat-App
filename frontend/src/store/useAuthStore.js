e30c867f0f3d41a5d3fa46dac7aed1b79a52c15f
MUHAMMED SIYAD T <146119724+Muhammedsiyadt@users.noreply.github.com>
2025-05-13 09:48:20 +0530

    changed url backend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://13.238.120.32:5001/api" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "/" : "http://13.238.120.32:5001/api";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
