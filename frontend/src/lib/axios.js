05d09c7d46dee3178f30a8e1ba40d046178eb7e7
MUHAMMED SIYAD T <146119724+Muhammedsiyadt@users.noreply.github.com>
2025-05-13 10:37:05 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
-import axios from "axios";
-
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "/" : "http://13.238.120.32:5001/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://13.238.120.32:5001/api"
+    : "https://13.238.120.32:5001/api", // ✅ Use HTTPS
   withCredentials: true,
 });
