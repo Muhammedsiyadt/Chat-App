508fc049df15fedfa123be0e905e2d7642658abb
MUHAMMED SIYAD T <146119724+Muhammedsiyadt@users.noreply.github.com>
2025-05-13 09:51:10 +0530

    s added axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "/" : "http://13.238.120.32:5001/api",
+  baseURL: import.meta.env.MODE === "development" ? "/" : "https://13.238.120.32:5001/api",
   withCredentials: true,
 });
